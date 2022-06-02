399c7bfdd8730fa1c7e3e7c251e622fe3da552a5
Falcon <88769526+buckdan@users.noreply.github.com>
2022-06-02 15:47:38 +0700

    Done some simple stuff

---
+++ 300475/Assets/Scripts/SinglePlayer/PlayerLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+
+	public float mouseSensitivity = 100f;
+
+	public Transform playerBody;
+
+	float xRotation = 0f;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.I)){
+			if(Cursor.lockState == CursorLockMode.Locked)
+				Cursor.lockState = CursorLockMode.None;
+			else if(Cursor.lockState == CursorLockMode.None)
+				Cursor.lockState = CursorLockMode.Locked;
+		}
+
+		float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+		float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+		xRotation -= mouseY;
+		xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+		transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+		playerBody.Rotate(Vector3.up * mouseX);
+	}
+}
